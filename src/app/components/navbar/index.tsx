4c24150ce4b63e9527462a0ff07b5878c85ee90f
iCarlosAugusto <augusto.dev@proton.me>
2024-04-30 00:25:22 -0300

    feat: :sparkles: fix navbar

--- src/app/components/navbar/index.tsx
+++ src/app/components/navbar/index.tsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 
 const Navbar = () => {
-  const onToggleMenu = (e: any) => {
-    const navLinks = document.querySelector('.nav-links');
-    e.target.name = e.target.name === 'menu' ? 'close' : 'menu';
-    navLinks?.classList.toggle('top-[0%]');
+
+  const onToggleMenu = () => { 
+    isModalOpen.current = !isModalOpen.current;
+    if(isModalOpen.current === true){
+      document.body.style.overflow = "hidden"
+    } else{
+      document.body.style.overflow = "visible"
+    }
+    containerMenuMobile.current?.classList.toggle('top-[0%]')
   };
+
+  const isModalOpen = useRef<boolean>(false);
+  const containerMenuMobile = useRef<HTMLDivElement>(null);
 
   return (
     <header>
@@ -13,8 +21,8 @@ const Navbar = () => {
         <div>
           <img className="w-16 cursor-pointer" src="https://cdn-icons-png.flaticon.com/512/5968/5968204.png" alt="Logo" />
         </div>
-        <div className="nav-links duration-500 bg-white md:static absolute md:min-h-fit min-h-[100vh] left-0 top-[-100%] md:w-auto w-full flex items-center px-5">
-        <button onClick={onToggleMenu} name="menu" className="text-3xl cursor-pointer md:hidden">Fechar</button>
+        <div ref={containerMenuMobile} className="nav-links duration-500 bg-white md:static absolute md:min-h-fit min-h-[100vh] left-0 top-[-100%] md:w-auto w-full flex items-center px-5">
+          <button onClick={onToggleMenu} className="text-3xl cursor-pointer md:hidden">Fechar</button>
           <ul className="flex md:flex-row flex-col md:items-center md:gap-[4vw] gap-8">
             <li>
               <a className="hover:text-gray-500" href="#">Products</a>
@@ -35,7 +43,7 @@ const Navbar = () => {
         </div>
         <div className="flex items-center gap-6">
           <button className="bg-[#a6c1ee] text-white px-5 py-2 rounded-full hover:bg-[#87acec]">Sign in</button>
-          <button onClick={onToggleMenu} name="menu" className="text-3xl cursor-pointer md:hidden">t</button>
+          <button onClick={onToggleMenu} className="text-3xl cursor-pointer md:hidden">Open</button>
         </div>
       </nav>
     </header>

7399641cadfda43d11e29287364cd0815b80c906
iCarlosAugusto <augusto.dev@proton.me>
2024-05-08 22:04:27 -0300

    feat: :sparkles: criado tela de auth

---
+++ src/app/pages/authentication/index.tsx
@@ -0,0 +1,32 @@
+import { Button } from "@/app/components/button"
+
+function Authentication() {
+
+
+    const handleAuth = () => {
+
+    }
+    return (
+        <div className="flex bg-red-300">
+            <div className="bg-green-200 w-2/4">
+                <span>foto</span>
+            </div>
+            <div className="flex flex-col p-10">
+                <div>
+                    <span>Entrar</span>
+                    <span>Cadastrar</span>
+                </div>
+
+                <span className="font-semibold text-3xl">Participe da maior comunidade de estudos de TI do Brasil!</span>
+
+                <input type="text" name="" id="" />
+                <input type="password" name="" id="" />
+                <button>Entrar</button>
+            
+                <span>Esqueci a senha</span>
+            </div>
+        </div>
+    )
+}
+
+export { Authentication }

48bea7dc6d36ab2b8e52327e8722b126d4bb46a7
iCarlosAugusto <augusto.dev@proton.me>
2024-05-19 19:19:09 -0300

    feat: :sparkles: input

--- src/components/modal/index.tsx
+++ src/components/modal/index.tsx
@@ -1,7 +1,17 @@
 import { Input } from "@/components/input"
-import { Button } from "../button"
+import { Button } from "../button";
+import { useActionState } from "react";
+import { createAccount } from "@/actions/createAccount.action";
+import { useFormState } from "react-dom";
 
 function ModalAuthentication() {
+
+    const initialState = {
+        message: ""
+    }
+
+    const [state, formAction] = useFormState(createAccount, initialState);
+    
     return (
         <div className="w-screen h-screen top-0 left-0 right-0 bottom-0 fixed z-50 flex items-center justify-center ">
             <div className="w-screen h-screen top-0 left-0 right-0 bottom-0 fixed bg-black opacity-80 flex items-center justify-center"></div>
@@ -18,12 +28,30 @@ function ModalAuthentication() {
                 </div>
 
                 <span className="mb-2 mt-2">ou</span>
-                
-                <Input placeholder="Seu nome" className="mb-4"/>
-                <Input placeholder="Seu email" className="mb-4" type="email"/>
-                <Input placeholder="Senha" className="mb-4" type="password"/>
-                <Button className="w-full md:w-full" onClick={() => {console.log("click")}} text="Criar conta"/>
-                <hr/>
+                <form action={formAction}>
+                    <Input
+                        placeholder="Seu nome"
+                        className="mb-4"
+                        name="username"
+                    />
+                    <Input
+                        placeholder="Seu email"
+                        className="mb-4"
+                        type="text"
+                        name="email"
+                    />
+                    <Input
+                        placeholder="Senha"
+                        className="mb-4"
+                        type="password"
+                        name="password"
+                    />
+                    <Button
+                        type="submit"
+                        className="w-full md:w-full"
+                        text="Criar conta"
+                    />
+                </form>
 
                 <div className="mt-5">
                     <span>Já tem uma conta?</span>

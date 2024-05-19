e3ba5a9c0646e39c96fa8c9c9e27284f470ddabb
iCarlosAugusto <augusto.dev@proton.me>
2024-05-19 19:18:32 -0300

    refactor: :recycle: improve btn

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,15 +1,17 @@
 interface ButtonProps {
     text: string;
-    onClick: () => void;
-    isDisabled?: boolean
-    className?: string
+    onClick?: () => void;
+    isDisabled?: boolean;
+    className?: string;
+    type?:  "submit" | "reset" | "button" | undefined;
 }
 
-function Button({text, onClick, isDisabled = false, className }: ButtonProps) {
+function Button({text, onClick, isDisabled = false, type="button", className }: ButtonProps) {
     return (
         <button 
+            type={type}
             className={`w-full md:w-auto bg-orange-500 text-white p-3 rounded-lg ${isDisabled ? "opacity-50" : "opacity-100 hover:opacity-70 transition-opacity"} ${className}`}
-            onClick={() => isDisabled ? null : onClick()}
+            //onClick={() => isDisabled ? null : onClick?.()}
         >
             <span className="font-semibold">{text}</span>
         </button>

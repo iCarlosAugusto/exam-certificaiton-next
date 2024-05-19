4bea823b4c473ce90a96fb40d1702d60f9ee43d2
iCarlosAugusto <augusto.dev@proton.me>
2024-05-19 15:01:11 -0300

    feat: :sparkles: ui modal

--- src/components/modal/index.tsx
+++ src/components/modal/index.tsx
@@ -6,7 +6,7 @@ function ModalAuthentication() {
         <div className="w-screen h-screen top-0 left-0 right-0 bottom-0 fixed z-50 flex items-center justify-center ">
             <div className="w-screen h-screen top-0 left-0 right-0 bottom-0 fixed bg-black opacity-80 flex items-center justify-center"></div>
             <div className="absolute bg-white p-10 flex flex-col items-center ml-5 mr-5 rounded">
-                <span>Crie sua conta!</span>
+                <span className="self-start font-bold text-2xl text-orange-500">Crie sua conta</span>
                 
                 <span>Seja bem-vindo! É um prazer ter você aqui :) <br/>Comece hoje a estudar gratuitamente</span>
 
@@ -24,7 +24,11 @@ function ModalAuthentication() {
                 <Input placeholder="Senha" className="mb-4" type="password"/>
                 <Button className="w-full md:w-full" onClick={() => {console.log("click")}} text="Criar conta"/>
                 <hr/>
-                <span>Crie sua conta</span>
+
+                <div className="mt-5">
+                    <span>Já tem uma conta?</span>
+                    <span className="font-bold ml-1 cursor-pointer">Entre por aqui</span>
+                </div>
             </div>
         </div>
     )

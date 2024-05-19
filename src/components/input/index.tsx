5171d705321d8871cd8de30780395978ad0b9169
iCarlosAugusto <augusto.dev@proton.me>
2024-05-19 19:18:53 -0300

    feat: :sparkles: improva input

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -4,12 +4,14 @@ interface InputProps {
     placeholder?: string;
     className?: string;
     type?: HTMLInputTypeAttribute;
+    name?: string;
 }
 
-function Input({placeholder, className, type="text"}: InputProps) {
+function Input({placeholder, className, name, type="text"}: InputProps) {
     return (
         <input 
-            type={type} 
+            type={type}
+            name={name} 
             className={`bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 ${className}`}
             placeholder={placeholder}
         />

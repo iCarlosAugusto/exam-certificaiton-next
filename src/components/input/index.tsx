3547820221c563b353562abe36804ec00f625b23
iCarlosAugusto <augusto.dev@proton.me>
2024-05-19 14:46:13 -0300

    feat: :sparkles: melhorado input

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,14 +1,19 @@
+import { HTMLInputTypeAttribute } from "react";
+
 interface InputProps {
     placeholder?: string;
+    className?: string;
+    type?: HTMLInputTypeAttribute;
 }
 
-function Input({placeholder}: InputProps) {
+function Input({placeholder, className, type="text"}: InputProps) {
     return (
-        <div className="mb-6">
-            <input type="text" id="default-input" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
-                placeholder={placeholder}
-            />
-        </div>
+        <input 
+            type={type} 
+            className={`bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 ${className}`}
+            placeholder={placeholder}
+        />
+
     )
 }
 

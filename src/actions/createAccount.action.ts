e53b1e9d5bf17b64a1c16c06a71680b2befc91e5
iCarlosAugusto <augusto.dev@proton.me>
2024-05-19 19:18:42 -0300

    feat: :sparkles: action

---
+++ src/actions/createAccount.action.ts
@@ -0,0 +1,15 @@
+"use server"
+
+export async function createAccount(
+    prevState: {
+      message: string;
+    },
+    formData: FormData,
+  ) {
+    console.log(formData);
+    try {
+      return { message: `Added todo ` };
+    } catch (e) {
+      return { message: "Failed to create todo" };
+    }
+  }

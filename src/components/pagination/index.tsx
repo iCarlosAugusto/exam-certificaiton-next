5ad36ba1c9882aaeb685ed16b659d3f990e14d96
iCarlosAugusto <augusto.dev@proton.me>
2024-05-19 21:56:10 -0300

    feat: :sparkles: improve pagination

--- src/components/pagination/index.tsx
+++ src/components/pagination/index.tsx
@@ -1,18 +1,14 @@
 "use client";
 
 import Link from "next/link";
-import { useRouter, useParams, usePathname, useSearchParams } from "next/navigation";
+import { usePathname, useSearchParams } from "next/navigation";
 import { useCallback } from "react";
 
-
-
 function Pagination() {
-    const router = useRouter();
-    const params = useParams();
     const path = usePathname();
     const searchParams = useSearchParams();
 
-
+    const currentPage = searchParams.get("page") ?? "1";
     const createQueryString = useCallback(
         (name: string, value: string) => {
           const params = new URLSearchParams(searchParams.toString())
@@ -27,13 +23,10 @@ function Pagination() {
         <div className="mt-5 mb-5 flex justify-center items-center">
             {[1,2,3,4,5].map(el => (
                 <Link href={path + '?' + createQueryString('page', el.toString())}>
-
-                <div 
-                    className="rounded-full items-center justify-center w-10 h-10 bg-slate-200 flex flex-row m-2 cursor-pointer"
-                    >
-                    <span>
-                        {el.toString()}
-                    </span>
+                    <div className={`border-2 border-slate-200 ${currentPage === el.toString() ? 'bg-slate-200' : 'bg-white'} rounded-full items-center justify-center w-10 h-10 flex flex-row m-2 cursor-pointer`}>
+                        <span>
+                            {el.toString()}
+                        </span>
                     </div>
                 </Link>
             ))}

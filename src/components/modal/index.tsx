bfdbe30affe2db218924122799855677cb669a14
iCarlosAugusto <augusto.dev@proton.me>
2024-05-24 23:27:00 -0300

    refactor: :recycle: refactor modal authentication

--- src/components/modal/index.tsx
+++ src/components/modal/index.tsx
@@ -2,17 +2,20 @@
 
 import { Input } from "@/components/input";
 import { Button } from "../button";
-import { Fragment, useActionState } from "react";
+import { Fragment, useActionState, useState } from "react";
 import { createAccount } from "@/actions/createAccount.action";
 import { useFormState } from "react-dom";
 import { useAuthentication } from "@/hooks/useAuthentication";
 import { LoginWithGoogle } from "../login-social/loginWithGoogle";
+import { IconX } from '@tabler/icons-react';
+
 
 interface ModalAuthenticationProps {
   isOpen: boolean;
+  closeModal: () => void;
 }
 
-function ModalAuthentication({ isOpen }: ModalAuthenticationProps) {
+function ModalAuthentication({ isOpen, closeModal }: ModalAuthenticationProps) {
   const initialState = {
     message: "",
   };
@@ -20,12 +23,25 @@ function ModalAuthentication({ isOpen }: ModalAuthenticationProps) {
   const [state, formAction] = useFormState(createAccount, initialState);
   const { authenticateWithGoogle, authenticateWithGithub, isLoading } = useAuthentication();
 
+  const handleCloseModal = () => closeModal();
+  
+  if(isOpen){
+    document.body.style.overflow = "hidden"
+  } else{
+    document.body.style.overflow = "visible"
+  }
+
   return (
     <Fragment>
       {isOpen && (
-        <div className="w-screen h-screen top-0 left-0 right-0 bottom-0 fixed z-50 flex items-center justify-center ">
-          <div className="w-screen h-screen top-0 left-0 right-0 bottom-0 fixed bg-black opacity-80 flex items-center justify-center"></div>
+        <div className="w-screen h-screen top-0 left-0 right-0 bottom-0 fixed z-50 flex items-center justify-center" >
+          <div className="w-screen h-screen top-0 left-0 right-0 bottom-0 fixed bg-black opacity-80 flex items-center justify-center" onClick={handleCloseModal}></div>
           <div className="absolute bg-white p-10 flex flex-col items-center ml-5 mr-5 rounded">
+          <div className="p-3 bg-slate-200 rounded-full absolute right-4 top-4 cursor-pointer" onClick={handleCloseModal}>
+            <IconX
+              size={24}
+            />
+          </div>
             <span className="self-start font-bold text-2xl text-orange-500">
               Crie sua conta
             </span>
@@ -44,7 +60,7 @@ function ModalAuthentication({ isOpen }: ModalAuthenticationProps) {
             </div>
 
             <span className="mb-2 mt-2">ou</span>
-            <form action={formAction}>
+            <form action={formAction} className="w-full">
               <Input placeholder="Seu nome" className="mb-4" name="username" />
               <Input
                 placeholder="Seu email"
@@ -54,7 +70,7 @@ function ModalAuthentication({ isOpen }: ModalAuthenticationProps) {
               />
               <Input
                 placeholder="Senha"
-                className="mb-4"
+                className="mb-4 w-full"
                 type="password"
                 name="password"
               />

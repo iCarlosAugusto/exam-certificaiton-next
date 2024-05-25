45116c272c75053f45f0d420187bc809a86daca0
iCarlosAugusto <augusto.dev@proton.me>
2024-05-24 23:27:45 -0300

    feat: :sparkles: change id

--- src/app/questions/page.tsx
+++ src/app/questions/page.tsx
@@ -1,5 +1,4 @@
 import { getQuestionsAction } from "@/actions/getQuestions.action";
-import { ModalAuthentication } from "@/components/modal";
 import { Pagination } from "@/components/pagination";
 import { QuestionSingle } from "@/components/question";
 import { QuestionMultiple } from "@/components/questionMultiple";
@@ -17,12 +16,12 @@ export default async function Questions({searchParams }: {
   }) {
     const currentPage = Number(searchParams?.page ?? "0");
     const questions: Pageable<QuestionEntity[]> = await getQuestionsAction(
-        "e87164d1-fb2e-4c5e-b361-00634a895962",
+        "c33a59d7-6770-4359-8bfa-1c199100f794",
         currentPage !== 0 ? currentPage - 1 : currentPage
     );
     return (
         <Fragment>
-            <ModalAuthentication isOpen={false}/>
+
             <main className="px-6 md:px-32 lg:px-72">
                 <h1>Questões AWS Cloud Patricioner TOTAL PAGES: {questions.totalPages}</h1>
                 <Search/>

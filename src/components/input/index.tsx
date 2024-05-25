69b584a30c7a0c7fd652006cae35322eaa6138c1
iCarlosAugusto <augusto.dev@proton.me>
2024-05-25 01:28:24 -0300

    refactor:

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,20 +1,26 @@
-import { HTMLInputTypeAttribute } from "react";
+import { Fragment, HTMLInputTypeAttribute } from "react";
 
 interface InputProps {
     placeholder?: string;
     className?: string;
     type?: HTMLInputTypeAttribute;
     name?: string;
+    error?: string;
 }
 
-function Input({placeholder, className, name, type="text"}: InputProps) {
+function Input({placeholder, className, name, error, type="text"}: InputProps) {
     return (
-        <input 
-            type={type}
-            name={name} 
-            className={`bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 ${className}`}
-            placeholder={placeholder}
-        />
+        <div className="mb-4">
+            <input 
+                type={type}
+                name={name} 
+                className={`bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 ${className}`}
+                placeholder={placeholder}
+            />
+            {error && (
+                <span className="text-red-500 font-bold">{error}</span>
+            )}
+        </div>
 
     )
 }

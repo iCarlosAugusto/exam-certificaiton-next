4864b7ed9880b89204edf3ab80b2cb491ddb54c5
iCarlosAugusto <augusto.dev@proton.me>
2024-07-05 19:24:03 -0300

    usando nextui

--- src/actions/getQuestions.action.ts
+++ src/actions/getQuestions.action.ts
@@ -1,12 +1,12 @@
-"use server";
+'use server';
 
-import { httpRequest } from "@/api/axios.helper";
-import { Pageable } from "@/entities/pagable.interface";
-import { QuestionEntity } from "@/entities/question.entity";
-
+import { httpRequest } from '@/api/axios.helper';
+import { Pageable } from '@/entities/pagable.interface';
+import { QuestionEntity } from '@/entities/question.entity';
 
 export const getQuestionsAction = async (courseId: string, page: number) => {
-    const { data } = await httpRequest.get<Pageable<QuestionEntity[]>>(`/courses/${courseId}/questions?page=${page}`);
-    return data;
-}
-
+  const { data } = await httpRequest.get<Pageable<QuestionEntity[]>>(
+    `/courses/${courseId}/questions?page=${page}`,
+  );
+  return data;
+};
--- src/app/questions/page.tsx
+++ src/app/questions/page.tsx
@@ -1,37 +1,43 @@
-import { getQuestionsAction } from "@/actions/getQuestions.action";
-import { PaginationComponent } from "@/components/pagination";
-import { QuestionSingle } from "@/components/question";
-import { QuestionMultiple } from "@/components/questionMultiple";
-import { Search } from "@/components/search";
-import { Pageable } from "@/entities/pagable.interface";
-import { QuestionEntity, QuestionType } from "@/entities/question.entity";
-import { Fragment } from "react/jsx-runtime";
+import { Fragment } from 'react/jsx-runtime';
 
+import { getQuestionsAction } from '@/actions/getQuestions.action';
+import { PaginationComponent } from '@/components/pagination';
+import { QuestionSingle } from '@/components/question';
+import { QuestionMultiple } from '@/components/questionMultiple';
+import { Search } from '@/components/search';
+import { Pageable } from '@/entities/pagable.interface';
+import { QuestionEntity, QuestionType } from '@/entities/question.entity';
 
-export default async function Questions({searchParams }: {
-    searchParams?: {
-      query?: string;
-      page?: string;
-    };
-  }) {
-    const currentPage = Number(searchParams?.page ?? "0");
-    const questions: Pageable<QuestionEntity[]> = await getQuestionsAction(
-        "c33a59d7-6770-4359-8bfa-1c199100f794",
-        currentPage !== 0 ? currentPage - 1 : currentPage
-    );
-    return (
-        <Fragment>
+export default async function Questions({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) {
+  const currentPage = Number(searchParams?.page ?? '0');
+  const questions: Pageable<QuestionEntity[]> = await getQuestionsAction(
+    '66874ffc40f84b60aee16a5d',
+    currentPage !== 0 ? currentPage - 1 : currentPage,
+  );
+  return (
+    <Fragment>
+      <main className="px-6 md:px-32 lg:px-72">
+        <h1>
+          Questões AWS Cloud Patricioner TOTAL PAGES: {questions.totalPages}
+        </h1>
 
-            <main className="px-6 md:px-32 lg:px-72">
-                <h1>Questões AWS Cloud Patricioner TOTAL PAGES: {questions.totalPages}</h1>
-                <Search/>
-                {questions.content.map((question, index) => (
-                    question.questionType === QuestionType.multipleReplies 
-                        ? <QuestionMultiple {...question} key={index * Math.random()}/> 
-                        : <QuestionSingle {...question} key={index * Math.random()}/>
-                ))}
-                <PaginationComponent totalPages={questions.totalPages}/>
-            </main>
-        </Fragment>
-    )
+        <Search />
+        {questions.content.map((question, index) =>
+          question.questionType === QuestionType.multipleReplies ? (
+            <QuestionMultiple {...question} key={index * Math.random()} />
+          ) : (
+            <QuestionSingle {...question} key={index * Math.random()} />
+          ),
+        )}
+        <PaginationComponent totalPages={questions.totalPages} />
+      </main>
+    </Fragment>
+  );
 }
